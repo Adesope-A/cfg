09aef9a20d61f45c34421116701d3605ae40ef45
Adesope-A <35933834+Adesope-A@users.noreply.github.com>
2018-03-20 02:08:34 +0000

    linked pages to nav bar, carousel needs pictures. jquery will be updated for nav bar

--- Java/script.js
+++ Java/script.js
@@ -1,8 +1,26 @@
-$(window).on("scroll", function() {
-   if($(window).scrollTop()) {
-        $("nav").addClass("black");
+$(function(){
+  $('#carousel-example-generic').carousel();
+});
+
+
+$(window).scroll(function(){
+    if ($(window).scrollTop() >= 300) {
+       $('nav').addClass('fixed-header');
+    }
+    else {
+       $('nav').removeClass('fixed-header');
+    }
+});
+
+window.onscroll = function() {myFunction()};
+
+var header = document.getElementById("navigation");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
   }
-  else {
-        $("nav").removeClass("black");
-  }
-})
+}
